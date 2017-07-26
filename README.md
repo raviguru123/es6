0ec7a9aacd96b5fba8d9b9830748204883be2954
raviguru123 <raviguruiitr@gmail.com>
2017-07-26 12:29:27 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+I am using this respository to learn and practise ES6 fetaure and practice
+based on chapter Wise.
---
+++ chapter-1/test.js
@@ -0,0 +1,5 @@
+//console.log("hello this is my first program");
+var a=10;
+var b=20;
+var c=a+b;
+print("hello",c);
